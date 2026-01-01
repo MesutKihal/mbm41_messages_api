676cf3319347b70ed97146c41fab39b4a0731b18
messaoudkihal <messaoudkihal@protonmail.com>
2026-01-01 23:18:25 +0100

    5th commit

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,11 @@
 
-from rest_framework.routers import DefaultRouter
+from django.conf import settings
 from django.urls import path
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.home),
     path('send/', views.send),
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- mbm41_messages/settings.py
+++ mbm41_messages/settings.py
@@ -34,6 +34,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'mbm41_messages.urls'
@@ -97,13 +98,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.2/howto/static-files/
-
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_ROOT = "staticfiles/"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
